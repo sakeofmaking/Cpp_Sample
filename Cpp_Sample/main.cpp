372b16437324c136456deaad7afad0a2f6f16a98
Nic La <thinklem@Nics-MacBook-Pro.local>
2015-11-01 15:56:15 -0800

    Initial Commit

---
+++ Cpp_Sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_Sample
+//
+//  Created by Nic La on 11/1/15.
+//  Copyright (c) 2015 Nic La. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
