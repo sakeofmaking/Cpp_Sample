e9a17c1ccc910a0855cbd641c0177957aef1779f
Nic La <thinklem@Nics-MacBook-Pro.local>
2015-11-01 15:59:26 -0800

    Initial Commit

--- Cpp_Sample/main.cpp
+++ Cpp_Sample/main.cpp
@@ -1,15 +1,25 @@
-//
-//  main.cpp
-//  Cpp_Sample
-//
-//  Created by Nic La on 11/1/15.
-//  Copyright (c) 2015 Nic La. All rights reserved.
-//
+/******************************************************************
+ * Title: Tic Tac Toe
+ *
+ * Purpose: Write a two-player tic-tac-toe game, allowing two humans
+ * to play against each other; use enums when possible to represent the values of the board.
+ *
+ ******************************************************************/
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+//Note TTTS_BLANK = 0, TTTS_O = 1, TTTS_X = 2
+enum TitTacToeSquare {TTTS_BLANK, TTTS_O, TTTS_X};
+
+int main(){
+    int board_position = 0;
+    
+    cout << "Player 1 turn: ";
+    cin >> board_position;
+    
+    if(board_position == TTTS_O){
+        cout << TTTS_O << "\n";
+    }
 }
