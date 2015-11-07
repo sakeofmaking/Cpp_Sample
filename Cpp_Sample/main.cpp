bbf339eaee671ca479a47cfff0c249c78f8837d4
Nic La <thinklem@Nics-MacBook-Pro.local>
2015-11-07 14:36:52 -0800

    TicTacToe

--- Cpp_Sample/main.cpp
+++ Cpp_Sample/main.cpp
@@ -2,7 +2,7 @@
  * Title: Tic Tac Toe
  *
  * Purpose: Write a two-player tic-tac-toe game, allowing two humans
- * to play against each other; use enums when possible to represent the values of the board.
+ * to play against each other
  *
  ******************************************************************/
 
@@ -10,16 +10,40 @@
 
 using namespace std;
 
-//Note TTTS_BLANK = 0, TTTS_O = 1, TTTS_X = 2
-enum TitTacToeSquare {TTTS_BLANK, TTTS_O, TTTS_X};
+void print_board(char spot[]){
+    cout << " " << spot[0] << " | " << spot[1] << " | " << spot[2] << " \n";
+    cout << "-----------\n";
+    cout << " " << spot[3] << " | " << spot[4] << " | " << spot[5] << " \n";
+    cout << "-----------\n";
+    cout << " " << spot[6] << " | " << spot[7] << " | " << spot[8] << " \n";
+}
 
 int main(){
-    int board_position = 0;
+    char board_position;
+    char board[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    bool turn = 0;
     
-    cout << "Player 1 turn: ";
-    cin >> board_position;
+    print_board(board);
     
-    if(board_position == TTTS_O){
-        cout << TTTS_O << "\n";
+    while(board_position != 'q'){
+        turn = !turn;
+        
+        if(turn){
+            cout << "Player X turn: ";
+            cin >> board_position;
+            if(board_position == 'q'){
+                break;
+            }
+            board[board_position - 49] = 'X';
+        } else{
+            cout << "Player O turn: ";
+            cin >> board_position;
+            if(board_position == 'q'){
+                break;
+            }
+            board[board_position - 49] = 'O';
+        }
+        print_board(board);
     }
+    
 }
