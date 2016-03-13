edac229ec91a3694d5f8fabf1859a2754fabf0f5
Nic La <thinklem@Nics-MacBook-Pro.local>
2016-03-12 16:47:56 -0800

    Detect Sort

--- Cpp_Sample/main.cpp
+++ Cpp_Sample/main.cpp
@@ -1,75 +1,112 @@
 /******************************************************************
- * Title: Highest Lowest Average
+ * Title: Detect Sort
  *
- * Prompt: Write a program that takes in 50 values and prints out the 
- * highest, the lowest, the average and then all 50 input values, one per line.
+ * Prompt: Write a program that detects whether an array is sorted or not, and if it is not sorted, sort it.
  *
  * Source: Allain, Alex (2013-09-16). Jumping into C++ (p. 147). Cprogramming.com. Kindle Edition.
  *
- * Warning: For simplification, input array is set between 10 and 99
+ * Note: findSmallestRemainingElement, swap, sort, displayArray functions from "Jumping into C++"
+ *
  ******************************************************************/
 
-#include <iostream>
-#include <ctime>        // For random seed
 #include <cstdlib>
-
-#define SIZE 50
+#include <iostream>
 
 using namespace std;
 
-// Generates a random number between low and high
-int randRange(int low, int high)
-{
-    return rand() % (high - low + 1) + low;
-}
+int findSmallestRemainingElement(int array[], int size, int index);
+void swap(int array[], int first_index, int second_index);
 
-// Prints the highest, lowest, average, entire array
-void printHLA(int randArray[SIZE])
+void sort(int array[], int size)
 {
-    int i, j;
-    int highest = 0;
-    int lowest = 100;
-    int sum = 0;
-    
-    // Print highest value
-    for(i = 0; i < SIZE; i++)
+    for(int i = 0; i < size; i++)
     {
-        if(randArray[i] > highest)
-        {
-            highest = randArray[i];
-        }
-        if(randArray[i] < lowest)
-        {
-            lowest = randArray[i];
-        }
-        sum += randArray[i];
-    }
-    cout << "Highest = " << highest << '\n';
-    cout << "Lowest = " << lowest << '\n';
-    cout << "Average = " << sum / SIZE << '\n';
-    
-    // Print entire array
-    cout << "Entire array:\n";
-    for(j = 0; j < SIZE; j++)
-    {
-        cout << randArray[j] << '\n';
+        int index = findSmallestRemainingElement(array, size, i);
+        swap(array, i, index);
     }
 }
 
+int findSmallestRemainingElement(int array[], int size, int index)
+{
+    int index_of_smallest_value = index;
+    for(int i = index + 1; i < size; i++)
+    {
+        if(array[i] < array[index_of_smallest_value])
+        {
+            index_of_smallest_value = i;
+        }
+    }
+    return index_of_smallest_value;
+}
+
+void swap(int array[], int first_index, int second_index)
+{
+    int temp = array[first_index];
+    array[first_index] = array[second_index];
+    array[second_index] = temp;
+}
+
+// Small helper function to display the before and after arrays
+void displayArray(int array[], int size)
+{
+    cout << "{";
+    for(int i = 0; i < size; i++)
+    {
+        if(i != 0)
+        {
+            cout << ", ";
+        }
+        cout << array[i];
+    }
+    cout << "}";
+}
+
+int detect_sorted(int array[], int size)
+{
+    int compare = 0;
+    int i;
+    
+    for(i = 0; i < size; i++)
+    {
+        if(compare > array[i])
+        {
+            return(1);
+        }
+        else
+        {
+            compare = array[i];
+        }
+    }
+    
+    return(0);
+}
 
 int main()
 {
-    // Seed srand
-    srand((int)time(NULL));
+    int size = 10;
+    int array[] = {1, 2, 3, 5, 4, 6, 7, 8, 9, 10};
     
-    // Create array of 50 random int
-    int randArray[SIZE];
-    for(int i = 0; i < SIZE; i++)
+    // Print original array
+    cout << "Original array: ";
+    displayArray(array, size);
+    cout << '\n';
+    
+    // Detect if array is sorted
+    if(detect_sorted(array, size) == 1)
     {
-        randArray[i] = randRange(10, 99);
+        cout << "Original array is NOT sorted\n";
+        
+        sort(array, size);
+        
+        // Print sorted array
+        cout << "Sorted array: ";
+        displayArray(array, size);
+        cout << '\n';
     }
-    
-    printHLA(randArray);
+    else
+    {
+        cout << "Original array is sorted\n";
+    }
     
 }
 
